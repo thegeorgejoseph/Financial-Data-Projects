756c05cf8a92ece6c87c3ae943e61513d2882f04
George Joseph <thegeorgejoseph@gmail.com>
2022-02-11 16:44:26 -0800

    adding highcharts api + news api calls

--- hw6/server.py
+++ hw6/server.py
@@ -1,9 +1,12 @@
 from curses import start_color
-from flask import Flask, Request
+from flask import Flask, Request, jsonify
 from dotenv import load_dotenv
 import os
 import json
 import requests
+from dateutil.relativedelta import relativedelta
+from datetime import *
+import time
 app = Flask(__name__)
 
 load_dotenv()
@@ -22,8 +25,10 @@ def finnhubRouter():
     response["Quote"] = quoteInfo
     recommendationInfo = recommendationAPI(mock_ticker)
     response["Recommendation"] = recommendationInfo
-    
-    return json.dumps(response)
+    response["Charts"] = highchartsAPI(mock_ticker)
+    newsList = latestNewsAPI(mock_ticker)
+    response["News"] = newsList
+    return jsonify(response)
 
 def companyURL2API(TICKER):
     addonString = f"stock/profile2?symbol={TICKER}&token={API_KEY}"
@@ -36,6 +41,7 @@ def companyURL2API(TICKER):
     companyInfo["Company IPO Date"] = res["exchange"]
     companyInfo["Category"] = res["finnhubIndustry"]
     
+      
     return companyInfo
 
 
@@ -70,6 +76,32 @@ def recommendationAPI(TICKER):
     
     return recommendation
 
+def highchartsAPI(TICKER):
+    charts = {}
+    now = datetime.now()
+    today = int(time.mktime(now.timetuple()))
+    temp = now + relativedelta(months=-6)
+    six_month_ago = int(time.mktime(temp.timetuple()))
+    addonString = f"stock/candle?symbol={TICKER}&resolution=D&from={six_month_ago}&to={today}&token={API_KEY}"
+    req = requests.get(BASE_URL + addonString)
+    res = req.json()
+    charts["Date"] = res["t"]
+    charts["Stock Price"] = res["c"]
+    charts["Volume"] = res["v"]
+    
+    return charts
 
+def latestNewsAPI(TICKER):
+    news = []
+    now = datetime.now()
+    today = now.strftime('%Y-%m-%d')
+    temp = now + relativedelta(months=-1)
+    one_month_ago = temp.strftime('%Y-%m-%d')
+    addonString = f"company-news?symbol={TICKER}&from={one_month_ago}&to={today}&token={API_KEY}"
+    req = requests.get(BASE_URL + addonString)
+    res = req.json()
+    for obj in res:
+        news.append({"Image":obj["image"],"Title":obj["headline"],"Date":obj["datetime"],"Link to Original Post": obj["url"]})
+    return news
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=81)
