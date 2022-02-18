20346946e60291311be25a3675561d47a2c1821f
George Joseph <thegeorgejoseph@gmail.com>
2022-02-18 12:52:43 -0800

    fixing green arrow edge case when change = 0

--- hw6/js/app.js
+++ hw6/js/app.js
@@ -112,8 +112,8 @@ function stockSummary() {
     monthNames[tradingDate.getMonth()]
   }, ${tradingDate.getFullYear()}`;
   recommendation = dataStore["Recommendation"];
-  redArrow = `<img src='img/RedArrowDown.png' width='10' height='10'></img>`;
-  greenArrow = `<img src='img/GreenArrowUp.png' width='10' height='10'>`;
+  redArrow = `<img id="redArrow" src='img/RedArrowDown.png' width='10' height='10'></img>`;
+  greenArrow = `<img id="greenArrow" src='img/GreenArrowUp.png' width='10' height='10'></img>`;
   html = `<table id="stats" ><tr><th align='right'>Stock Ticker Symbol</th><th align='left' id="lighweight">${profile_ticker}</th></tr>
   <tr><th align='right'>Trading Day</th><th align='left' id="lighweight">${tradingDateString}</th></tr>
   <tr><th align='right'>Previous Closing Price</th><th align='left' id="lighweight">${
@@ -144,6 +144,10 @@ function stockSummary() {
   }</th><th id="sb-text" class="lighweight">Strong Buy</th></tr></table>
   <h3>Recommendation Trends</h3>`;
   stocksElement.innerHTML = html;
+  if (summary["Change"] == 0 && summary["Change Percent"] == 0) {
+    document.querySelectorAll("#greenArrow")[0].style.display = "none";
+    document.querySelectorAll("#greenArrow")[1].style.display = "none";
+  }
 }
 
 function getCharts() {
