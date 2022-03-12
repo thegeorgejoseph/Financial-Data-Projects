daa1c2cff255d35b76ec2bf7c17cbb155f286fcc
George Joseph <thegeorgejoseph@gmail.com>
2022-03-12 14:39:02 -0800

    Navbar style complete

--- hw8/finnhub-angular/src/app/app.module.ts
+++ hw8/finnhub-angular/src/app/app.module.ts
@@ -6,11 +6,13 @@ import { AppComponent } from './app.component';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
--- hw8/finnhub-angular/src/app/components/navbar/navbar.component.ts
+++ hw8/finnhub-angular/src/app/components/navbar/navbar.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  styleUrls: ['./navbar.component.css'],
 })
 export class NavbarComponent implements OnInit {
+  isPrimary: string;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  changeLinkNumber1(): void {
+    this.isPrimary = '1';
   }
-
+  changeLinkNumber2(): void {
+    this.isPrimary = '2';
+  }
+  changeLinkNumber3(): void {
+    this.isPrimary = '3';
+  }
 }
