d592e977664fa021f643cb80c2cc76d344a1c8d5
George Joseph <thegeorgejoseph@gmail.com>
2022-03-12 16:05:13 -0800

    Creating Footer and defining viewport height

--- hw8/finnhub-angular/src/app/app.module.ts
+++ hw8/finnhub-angular/src/app/app.module.ts
@@ -7,20 +7,36 @@ import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { WatchlistComponent } from './components/watchlist/watchlist.component';
+import { PortfolioComponent } from './components/portfolio/portfolio.component';
+import { RouterModule, Routes } from '@angular/router';
+import { SearchComponent } from './components/search/search.component';
+
+const appRoutes: Routes = [
+  { path: '', component: SearchComponent },
+  { path: 'search', component: SearchComponent },
+  { path: 'watchlist', component: WatchlistComponent },
+  { path: 'portfolio', component: PortfolioComponent },
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    FooterComponent
+    FooterComponent,
+    WatchlistComponent,
+    PortfolioComponent,
+    SearchComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     NoopAnimationsModule,
-    NgbModule
+    NgbModule,
+    RouterModule.forRoot(appRoutes, { enableTracing: true }),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  exports: [RouterModule],
 })
-export class AppModule { }
+export class AppModule {}
