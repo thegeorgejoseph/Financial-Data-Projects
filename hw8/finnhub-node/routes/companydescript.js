b210959932a769a2fa716ace8e1721344d23d7be
George Joseph <thegeorgejoseph@gmail.com>
2022-03-18 16:34:16 -0700

    Modifying logic to throw error in company description

--- hw8/finnhub-node/routes/companydescript.js
+++ hw8/finnhub-node/routes/companydescript.js
@@ -11,19 +11,19 @@ router.get("/:ticker", (req, res) => {
       `https://finnhub.io/api/v1/stock/profile2?symbol=${req.params.ticker}&token=${process.env.API_URL}`
     ),
     axios.get(
-      `https://finnhub.io/api/v1/quote?symbol=${req.params.ticker}&token=${process.env.API_URL}`
+      `https://finnhub.io/api/v1/quote?symbol=${req.params.ticker}&token=${process.env.API_URL}` // double check that quote api gets all caps as input for it to work!
     ),
   ])
     .then((responses) => {
       responses.map((res) => {
         current = res.data;
+        console.log(current);
+        if (Object.keys(current).length === 0) {
+          throw "Invalid Ticker";
+        }
         dataObj = { ...dataObj, ...current };
       });
-      if (dataObj.d === null && dataObj.dp === null && dataObj.t === 0) {
-        throw "Invalid Ticker";
-      } else {
-        res.status(200).json(dataObj);
-      }
+      res.status(200).json(dataObj);
     })
     .catch((err) => {
       res.status(404).json({ msg: err });
