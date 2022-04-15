93a2735f85f27ae0baac7ee8f174713c969c9757
George Joseph <thegeorgejoseph@gmail.com>
2022-04-14 17:15:18 -0700

    Setting AppIcon

---
+++ hw9/finnhub-ios-node/finnhub-ios-node/finnhub_ios_nodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  finnhub_ios_nodeApp.swift
+//  finnhub-ios-node
+//
+//  Created by George Joseph on 4/14/22.
+//
+
+import SwiftUI
+
+@main
+struct finnhub_ios_nodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
